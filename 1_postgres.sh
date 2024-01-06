f10d82f1b150977247c853b662c843aa14be0b2b
marat2010 <marat.s.73@mail.ru>
2024-01-06 23:44:20 +0300

    Ok

--- 1_postgres.sh
+++ 1_postgres.sh
@@ -40,7 +40,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 #==========================
 #sudo usermod -aG docker $USER
