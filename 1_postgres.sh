8b8255045ef6b31ff23447d7e22893ed419ef3ad
marat2010 <marat.s.73@mail.ru>
2024-01-07 06:36:26 +0300

    Ok

--- 1_postgres.sh
+++ 1_postgres.sh
@@ -24,7 +24,7 @@ echo "=== Установка Docker, Docker-compose ==="
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get -y install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
